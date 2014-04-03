19208a342402d07e04be6da87d771830cfcb7014
Daniel Jurczak <daniel.jurczak@imba.oeaw.ac.at>
2014-04-03 12:43:05 +0200

    base version, generates windows via GenomeTiler#split_into_windows

---
+++ lib/genome_tiler.rb
@@ -0,0 +1,41 @@
+require 'stringio'
+require 'bio'
+
+class GenomeTiler
+  def split_into_windows(data, output, window_size, options)
+    items = 0
+
+    each_sequence_in_data(data, window_size) do |definition, sequence|
+      write_element_to_stream(output, definition, sequence)
+      items += 1
+    end
+
+    items
+  end
+
+  def each_sequence_in_data(data, window_size)
+    Bio::FlatFile.auto(data) do |ff|
+      ff.each do |entry|
+        definition = entry.definition
+        sequence = entry.seq
+        (0..sequence.length-window_size).each do |i|
+          yield generate_definition(definition, i, window_size), sequence[i..i+window_size]
+        end
+      end
+    end
+  end
+
+  def generate_definition(definition, position, window_size)
+    fields = definition_to_fields(definition)
+    ">#{fields["ID"].downcase}_#{position}_#{position+window_size}"
+  end
+
+  def definition_to_fields(definition)
+    definition.split(";").map { |s| s.strip.split("=") }.reduce({}) { |o, n| o.merge!(n[0] => n[1]) }
+  end
+
+  def write_element_to_stream(output, definition, sequence)
+    output.puts("#{definition}\n#{sequence}\n")
+  end
+end
+
---
+++ spec/lib/genome_tiler_spec.rb
@@ -0,0 +1,95 @@
+require_relative File.join('..', '..', 'lib', 'genome_tiler.rb')
+
+describe GenomeTiler do
+  let(:data) { File.join(File.dirname(__FILE__), '..', 'fixtures', 'sample_data.fasta') }
+
+  before(:each) do
+    @output = StringIO.new
+  end
+
+  context "#split_into_windows" do
+    it "takes a fasta file and splits its sequences into overlapping windows" do
+      instance = GenomeTiler.new
+      instance.split_into_windows(data, @output, 20, {})
+      expect(@output.string.split("\n").count).to eq 62
+    end
+
+    context "#each_sequence_in_data" do
+      it "yields control for each possible split for a sequence" do
+        instance = GenomeTiler.new
+        expect { |b| instance.each_sequence_in_data(data, 20, &b) }.to yield_control.exactly(31).times
+      end
+
+      it "yields a position-specific subsequence" do
+        i = 0
+        seqs = ["AGGGTCACGTAATGCTGATCC", "GGGTCACGTAATGCTGATCCA", "GGTCACGTAATGCTGATCCAG", "GTCACGTAATGCTGATCCAGT", "TCACGTAATGCTGATCCAGTC"]
+
+        instance = GenomeTiler.new
+        instance.each_sequence_in_data(data, 20) do |definition, seq|
+          expect(seq).to eq seqs[i]
+          i+=1
+          break if i == 4
+        end
+      end
+
+      it "contains the right chromosome and position in the definition line" do
+        i = 0
+        definitions = [">yhet_0_20", ">yhet_1_21", ">yhet_2_22", ">yhet_3_23", ">yhet_4_24"]
+
+        instance = GenomeTiler.new
+        instance.each_sequence_in_data(data, 20) do |definition, seq|
+          expect(definition).to eq definitions[i]
+          i+=1
+          break if i == 4
+        end
+      end
+
+      describe "#generate_definition" do
+        context "given a fasta definition line that contains an ID element" do
+          it "generates a definiton string with the ID and start/end position" do
+            instance = GenomeTiler.new
+            expect(instance.generate_definition(">YHet type=chromosome_arm; loc=YHet:1..347038; ID=YHet;", 5, 20)).to eq ">yhet_5_25"
+          end
+        end
+
+        describe "#definition_to_fields" do
+          context "given a fasta definition line we split it by ';' and map it into a Hash" do
+            it "??" do
+              instance = GenomeTiler.new
+              titles = [">YHet type", "loc", "ID", "dbxref", "MD5", "length", "release", "species"]
+              fields = instance.definition_to_fields(">YHet type=chromosome_arm; loc=YHet:1..347038; ID=YHet; dbxref=REFSEQ:NW_001848860,GB:CM000461; MD5=478fbc07ea1b1c03b3d8d04abacf51a5; length=347038; release=r5.51; species=Dmel;")
+              fields.keys.each do |key|
+                expect(titles.include?(key)).to be_true
+                titles.delete(key)
+              end
+            end
+          end
+        end
+
+      end
+
+    end
+
+    describe "#write_element_to_stream" do
+      it "writes the name of an transposon element to a stream" do
+        instance = GenomeTiler.new
+
+        output = StringIO.new
+        instance.write_element_to_stream(output, ">yhet_0_20", "AGAGAGAGAG")
+        output.close
+        expect(output.string.split("\n").first).to eq ">yhet_0_20"
+      end
+
+      it "writes the sequence of an transposon element to a stream" do
+        instance = GenomeTiler.new
+
+        output = StringIO.new
+        instance.write_element_to_stream(output, ">yhet_0_20", "AGAGAGAGAG")
+        output.close
+
+        expect(output.string.split("\n").last).to eq "AGAGAGAGAG"
+      end
+    end
+
+  end
+end
